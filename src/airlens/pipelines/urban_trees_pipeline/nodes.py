2925eceaa67520c9778e5d6a39e8db0406561cac
luisalopresti <luisa.loprest@gmail.com>
2025-06-09 18:20:54 +0100

    tree pipeline

---
+++ src/airlens/pipelines/urban_trees_pipeline/nodes.py
@@ -0,0 +1,50 @@
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import box
+from typing import Optional
+
+def trees_in_unit(tree_gdf: gpd.GeoDataFrame,
+                  air_gdf: gpd.GeoDataFrame):
+    '''
+    Assign Trees to Spatial Unit.
+    
+    Usea `within` for a one-to-one spatial join;
+    may use `intersects` to not lose trees at the intersections 
+    between spatial unit but multiple countings would occur.
+    '''
+    # assign IDs for tree geoms
+    tree_gdf['TreeID'] = range(1, len(tree_gdf)+1)
+
+    # reproject to same crs
+    air_geoms = air_gdf.to_crs(tree_gdf.crs)
+
+    # retain only area of interest
+    bbox = air_geoms.total_bounds
+    bbox_geom = box(*bbox)
+    tree_gdf = tree_gdf[tree_gdf.geometry.intersects(bbox_geom)].reset_index(drop=True)
+
+    # assign trees to spatial unit 
+    air_geoms = air_geoms[['SpatialUnitID', 'geometry']].copy()
+    return gpd.sjoin(tree_gdf, air_geoms, how='right', predicate='within')
+
+
+def tree_features(tree_to_unit_gdf: gpd.GeoDataFrame,
+                  air_gdf: gpd.GeoDataFrame,
+                  crs_metric: Optional[str] = "EPSG:3857"):
+    # ensure same metric crs
+    air_gdf = air_gdf.to_crs(crs_metric)
+    tree_to_unit_gdf = tree_to_unit_gdf.to_crs(air_gdf.crs)
+
+    # 1. compute number of trees for spatial unit
+    aggr_trees_per_unit = tree_to_unit_gdf.groupby('SpatialUnitID').agg(
+        total_trees_area=('area', 'sum'), # compute tot area covered by trees per unit
+        count_trees=('SpatialUnitID', 'size') # compute number of trees for spatial unit
+    ).reset_index()
+    tree_air_gdf = pd.merge(aggr_trees_per_unit, air_gdf, on='SpatialUnitID', how='right')
+    tree_air_gdf = gpd.GeoDataFrame(tree_air_gdf, geometry='geometry', crs=air_gdf.crs)
+
+    # 2. compute percentage of area covered by trees (canopy cover)
+    tree_air_gdf['canopy_cover'] = (tree_air_gdf.total_trees_area / tree_air_gdf.area) * 100
+    tree_air_gdf.drop(columns=['total_trees_area'], inplace=True)
+
+    return tree_air_gdf
---
+++ src/airlens/pipelines/urban_trees_pipeline/pipeline.py
@@ -0,0 +1,27 @@
+from kedro.pipeline import Pipeline, node
+from .nodes import trees_in_unit, tree_features
+
+def create_pipeline(**kwargs):
+    return Pipeline([
+        ## SJOIN TREE WITHIN SPATIAL UNIT
+        node(
+            func=trees_in_unit,
+            inputs={
+                "tree_gdf":"urban_atlas_street_trees",
+                "air_gdf":"ED_aggregated_air"
+                },
+            outputs="trees_in_units",
+            name="trees_in_units"
+        ),
+        ## COMPUTE TREES-RELATED COVARIATES
+        node(
+            func=tree_features,
+            inputs={
+                "tree_to_unit_gdf":"trees_in_units",
+                "air_gdf":"ED_aggregated_air",
+                "crs_metric":"params:crs_metric"
+            },
+            outputs="air_trees_features",
+            name="tree_features"
+        )
+    ])
